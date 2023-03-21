16970cb00941c204662b00e791666c9b5288269b
nin-jin <nin-jin@users.noreply.github.com>
2023-03-21 07:12:05 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2501,6 +2501,7 @@ declare namespace $ {
         Row(id: any): $$.$mol_text_code_row;
         rows(): readonly any[];
         Rows(): $$.$mol_list;
+        text_export(): string;
         Copy(): $$.$mol_button_copy;
     }
 }
@@ -2537,6 +2538,7 @@ declare namespace $.$$ {
         }>;
         uri_base(): string;
         uri_resolve(uri: string): string;
+        text_export(): string;
     }
 }
 
