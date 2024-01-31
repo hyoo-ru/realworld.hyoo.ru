a43e36cfb6bb32ee08a404982657d259b0e9d861
nin-jin <nin-jin@users.noreply.github.com>
2024-01-31 07:12:00 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1093,15 +1093,8 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    function $mol_diff_path<Item>(...paths: Item[][]): {
-        prefix: Item[];
-        suffix: Item[][];
-    };
-}
-
-declare namespace $ {
-    class $mol_error_mix extends Error {
-        errors: Error[];
+    class $mol_error_mix extends AggregateError {
+        name: string;
         constructor(message: string, ...errors: Error[]);
         toJSON(): string;
     }
@@ -1109,6 +1102,7 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_data_error extends $mol_error_mix {
+        name: string;
     }
 }
 
