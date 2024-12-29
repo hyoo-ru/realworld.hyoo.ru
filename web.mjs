0a92af4beb838c7b84550dd2053905a6152acd45
nin-jin <nin-jin@users.noreply.github.com>
2024-12-29 07:17:04 +0000

    Deploying to gh-pages from @ hyoo-ru/realworld.hyoo.ru@40f8e823e572bf5d6e77265779532e9aa5cfb0e1 🚀

--- web.mjs
+++ web.mjs
@@ -4738,7 +4738,7 @@ var $;
             Tools: {
                 flex: {
                     basis: 'auto',
-                    grow: 1000,
+                    grow: 0,
                     shrink: 1,
                 },
                 display: 'flex',
