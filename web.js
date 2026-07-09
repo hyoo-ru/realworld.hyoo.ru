53d1b52fa929121396d49cae9fbf21ca012acc05
nin-jin <nin-jin@users.noreply.github.com>
2026-07-09 10:26:39 +0000

    Deploying to gh-pages from @ hyoo-ru/realworld.hyoo.ru@40f8e823e572bf5d6e77265779532e9aa5cfb0e1 🚀

--- web.js
+++ web.js
@@ -12332,6 +12332,9 @@ var $;
 
 ;
 	($.$mol_form_field) = class $mol_form_field extends ($.$mol_labeler) {
+		state(){
+			return null;
+		}
 		name(){
 			return "";
 		}
@@ -12345,6 +12348,9 @@ var $;
 		}
 		control(){
 			return null;
+		}
+		attr(){
+			return {...(super.attr()), "mol_form_field_state": (this.state())};
 		}
 		bids(){
 			return [];
@@ -12373,6 +12379,9 @@ var $;
          * @see https://mol.hyoo.ru/#!section=demos/demo=mol_form_demo
          */
         class $mol_form_field extends $.$mol_form_field {
+            state() {
+                return this.bid() ? 'bid' : null;
+            }
             bid() {
                 return this.bids().filter(Boolean)[0] ?? '';
             }
